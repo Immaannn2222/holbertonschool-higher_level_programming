bf99ae0fc464aa0bab072f513af57c5294320722
Immaannn2222 <imennaayari@gmail.com>
2020-04-28 21:57:30 +0100

    fix

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    for x in str:
+        if (ord(x) > 96) and (ord(x) < 123):
+            y = ord(x) - 32
+        else:
+            y = ord(x)
+        print("{}".format(chr(y)), end="")
+    print("")
