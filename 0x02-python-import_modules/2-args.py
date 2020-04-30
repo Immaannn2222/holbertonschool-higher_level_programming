d832aea092005c9ad8bef766d17dd4832a4cbfc0
Immaannn2222 <imennaayari@gmail.com>
2020-04-30 04:06:08 +0100

    push without check

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import argv
+if len(argv) == 1:
+    print("0 arguments.")
+elif len(argv) != 2:
+    print("{:d} arguments:".format(len(argv) - 1))
+    for i in range(1, len(argv)):
+        print("{:d}: {}".format(i, argv[i]))
+else:
+    print("{:d} argument:".format(len(argv) - 1))
+    for i in range(1, len(argv)):
+        print("{:d}: {}".format(i, argv[i]))
---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+import variablae_load_5
+print("{}".format(variablae_load_5.a))
