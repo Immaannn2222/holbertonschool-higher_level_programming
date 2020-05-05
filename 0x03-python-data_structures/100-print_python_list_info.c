4170accb4310132d50838fb99609d6b1f3910d5e
Immaannn2222 <imennaayari@gmail.com>
2020-05-05 23:14:01 +0100

    file ready

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Python.h>
+/**
+ * print_python_list_info - prints some basic info about Python lists
+ * @p: pyobject
+ *
+ * Return: void
+ */
+void print_python_list_info(PyObject *p)
+{
+
+}
