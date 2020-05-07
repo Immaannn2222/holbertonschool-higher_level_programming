e4ff6a2102752c89d79d58b83aec470cc3ef97e6
Immaannn2222 <imennaayari@gmail.com>
2020-05-07 20:49:24 +0100

    swear last

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
-        return None
-    elif a_dictionary is {}:
-        return None
-    elif type(a_dictionary) is not dict:
-        return None
-    else:
+    if a_dictionary:
         return max(a_dictionary)
