b3d6a6a8eb3ad6b50d0f3895e8366c00f757cb62
Immaannn2222 <imennaayari@gmail.com>
2020-05-07 20:53:39 +0100

    out

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary and a_dictionary is not {}:
-        return max(a_dictionary)
+    if not a_dictionary:
+        return None
+    laureat = 0
+    for key, value in a_dictionary.items():
+        if value > laureat:
+            laureat = value
+    for key, value in a_dictionary.items():
+        if value == laureat:
+            return key
