84fa739d404d9f057e784d06b7ca9ec64f3f88ce
Immaannn2222 <imennaayari@gmail.com>
2020-05-07 04:06:32 +0100

    reched

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if matrix is not None:
+        new_matrix = []
+        for x in matrix:
+            new_matrix.append(list(map(lambda y: y**2, x)))
+        return new_matrix
---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    if set is not None or set_2 is not None:
+        c = set_1.intersection(set_2)
+    return c
