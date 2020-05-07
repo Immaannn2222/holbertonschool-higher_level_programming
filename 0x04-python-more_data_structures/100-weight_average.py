1019201e86f06f35fce8aa716fe32c688164104a
Immaannn2222 <imennaayari@gmail.com>
2020-05-07 19:00:34 +0100

    check adv 1

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list is [] or my_list is None:
+        return 0
+    sum = 0
+    average = 0
+    for i in my_list:
+        sum += i[0] * i[1]
+        average += i[1]
+        x = sum / average
+    return x
