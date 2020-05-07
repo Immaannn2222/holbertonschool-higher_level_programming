7898e7ae12aa05c346baef551bbee8fce9f1900a
Immaannn2222 <imennaayari@gmail.com>
2020-05-07 13:12:59 +0100

    fix 1 again

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     if my_list is not None:
-        for i in range(len(my_list) - 1):
-            if(my_list[i] == search):
-                my_list[i] = replace
-    return my_list
+        new = my_list[:]
+        for i in range(len(new)):
+            if(new[i] == search):
+                new[i] = replace
+    return new
