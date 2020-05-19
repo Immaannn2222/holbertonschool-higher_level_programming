51ab925ca757ce828efb3ca0ae53d98bbe8c03e0
Immaannn2222 <imennaayari@gmail.com>
2020-05-19 23:11:17 +0100

    check

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Python classes"""
+
+
+class Square():
+    """Print square"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        """square area"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """square size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter"""
+        if isinstance(value, int):
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def my_print(self):
+        """prints the square"""
+        for count1 in range(self.__size):
+            for count2 in range(self.__size):
+                print("#", end='')
+        if self.__size == 0:
+            print()
