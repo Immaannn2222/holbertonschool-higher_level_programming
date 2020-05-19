20987e0b01ea6fc83156fd18de36f1287fe76de7
Immaannn2222 <imennaayari@gmail.com>
2020-05-19 22:09:32 +0100

    fix

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Classes in Python"""
+
+
+class Square:
+    """ a square class that is empty"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Classes in Python"""
+
+
+class Square:
+    """ a square class with size """
+    def __init__(self, size):
+        self.__size = size
