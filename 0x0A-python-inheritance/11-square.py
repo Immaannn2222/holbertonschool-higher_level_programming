4647bf833468a34c3abd87163914278bcb6c2287
Immaannn2222 <imennaayari@gmail.com>
2020-06-02 19:59:28 +0100

    fix

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Inheritance"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """square class"""
+    def __init__(self, size):
+        """"Instantiation of size"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+
+    def __str__(self):
+        """prints"""
+        return("[Square] {}/{}".format(self.__size, self.__size))
