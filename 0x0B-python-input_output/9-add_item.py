b78b5ffe1bfb8527ae95ab8629baa011834fb143
Immaannn2222 <imennaayari@gmail.com>
2020-06-03 21:41:24 +0100

    fix

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 import json
 
 
 save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
-li = sys.argv[1:]
+
 
 try:
-    li = load_from_json_file("add_item.json")
+    t = load_from_json_file("add_item.json")
 except IOError:
-    li += sys.argv[1:]
-    save_to_json_file(old, "add_item.json")
+    t = []
+t += argv[1:]
+save_to_json_file(t, "add_item.json")
