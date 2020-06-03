e379a545d42241ac2116d9f1e662edf5a6bfe54b
Immaannn2222 <imennaayari@gmail.com>
2020-06-03 21:13:03 +0100

    fix

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Python I/O"""
+
+
+class Student:
+    """A student class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list) is not True:
+            return self.__dict__
+        else:
+            m = {}
+            for x in attrs:
+                    if type(x) is not str:
+                        return self.__dict__
+                    if x in self.__dict__:
+                        m[x] = self.__dict__[x]
+            return m
