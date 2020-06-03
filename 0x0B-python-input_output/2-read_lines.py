85a8ce02cce9a79f1b60f0399fcdc15d1c529e49
Immaannn2222 <imennaayari@gmail.com>
2020-06-03 20:15:39 +0100

    fix

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -3,11 +3,11 @@
 
 
 def read_lines(filename="", nb_lines=0):
-    """eads n lines of a text file"""
+    """reads n lines of a text file"""
     co = 0
-    with open(filename, "r") as fi:
-        for line in fi:
-            if co >= nb_lines or nb_lines < 0:
+    with open(filename) as fi:
+        li = fi.readlines()
+        for co in range(len(li)):
+            if co == nb_lines and nb_lines != 0:
                 break
-            co += 1
-            print(line, end="")
+            print(li[co], end="")
