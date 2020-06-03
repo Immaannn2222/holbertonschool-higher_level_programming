95084d2e70c7796a79ee14f0ebdab1ac5b8e570e
Immaannn2222 <imennaayari@gmail.com>
2020-06-03 22:23:36 +0100

    f

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -4,12 +4,13 @@
 
 def pascal_triangle(n):
     """list of lists of integers representing the Pascal’s triangle of n"""
+    triangle = []
     if n <= 0:
-        return
+        return triangle
 
+        triangle[0] = 1
     for l in range(1, n + 1):
-        c = 1
         for x in range(1, l + 1):
-            print(c, end=" ")
             c = int(c * (l - x) / x)
-        print("")
+        triangle[l].appen(c)
+    return triangle
