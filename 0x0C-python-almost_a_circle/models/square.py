5b633cc4284887e8c4676437f0004a30e47d2a9a
Immaannn2222 <imennaayari@gmail.com>
2020-06-09 14:29:49 +0100

    too much

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,8 +5,10 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """Class Square inherits from Rectangle"""
+
     def __init__(self, size, x=0, y=0, id=None):
         """Class constructor"""
+        self.size = size
         super().__init__(size, size, x, y, id)
 
     @property
