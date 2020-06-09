e818ada0a61ac5937e9082b36881b1e09ebe83e5
Immaannn2222 <imennaayari@gmail.com>
2020-06-09 15:23:59 +0100

    last

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,9 +7,8 @@ class Square(Rectangle):
     """Class Square inherits from Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
         """Class constructor"""
-        self.size = size
         super().__init__(size, size, x, y, id)
-    
+
     def __str__(self):
         """display"""
         return "[{}] ({}) {}/{} - {}/{}".format(type(self).__name__,
@@ -23,11 +22,6 @@ class Square(Rectangle):
 
     @size.setter
     def size(self, value):
-        """size setter."""
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be > 0")
         self.width = value
         self.height = value
 
