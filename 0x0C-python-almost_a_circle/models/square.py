86f751a97c1ef5f0ce647c2a9d85cdb9b8ec1c71
Immaannn2222 <imennaayari@gmail.com>
2020-06-09 16:05:27 +0100

    fix

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,9 +12,12 @@ class Square(Rectangle):
 
     def __str__(self):
         """display"""
-        return "[{}] ({}) {}/{} - {}/{}".format(type(self).__name__,
-                                                self.id, self.x, self.y,
-                                                self.width, self.height)
+        return "[{}] ({}) {}/{} - {}".format(
+            type(self).__name__,
+            self.id,
+            self.x,
+            self.y,
+            self.width)
 
     @property
     def size(self):
