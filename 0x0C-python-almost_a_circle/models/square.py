e26be0bdd08a42085431e3dab5123a949f625c29
Immaannn2222 <imennaayari@gmail.com>
2020-06-09 14:34:41 +0100

    too much

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,7 +8,7 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """Class constructor"""
-        self.__size = size
+        self.size = size
         super().__init__(size, size, x, y, id)
 
     @property
