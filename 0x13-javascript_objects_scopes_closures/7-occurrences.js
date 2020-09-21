7004c44d4e4a194069b87484214cc00bea430239
Immaannn2222 <imennaayari@gmail.com>
2020-09-21 14:41:49 +0100

    2nd batch

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let j = 0;
+  for (const i of list) {
+      if (i === searchElement)
+      {
+    j += 1;
+      }
+  }
+  return (j);
+};
