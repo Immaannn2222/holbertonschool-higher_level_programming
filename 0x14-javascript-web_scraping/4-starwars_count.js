35dfd39e16455d1bcc81229325efbd18a1ed4341
Immaannn2222 <imennaayari@gmail.com>
2020-09-22 16:26:24 +0100

    better version

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,8 +5,8 @@ request(U_R_L, function (error, response, body) {
   if (!error && response.statusCode === 200) {
     let i = 0;
     const resultt = JSON.parse(body).results;
-    for (let j = 0; j < resultt.length; j++) {
-      if (resultt[i].characters.find(x => x.includes('/api/people/18/'))) {
+    for (let j = 0; j < resultt.length - 1; j++) {
+      if (resultt[i].characters.find(x => x.includes('https://swapi-api.hbtn.io/api/people/18/'))) {
         i++;
       }
     }
