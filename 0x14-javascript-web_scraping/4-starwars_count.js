ee16f86b0fcffba5ee27ae5f737cff5b43981305
Immaannn2222 <imennaayari@gmail.com>
2020-09-22 21:04:57 +0100

    outpuit

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,11 +2,14 @@
 const request = require('request');
 const U_R_L = 'https://swapi-api.hbtn.io/api/films';
 request(U_R_L, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
+  if (error) {
+    console.log(error);
+  } else if ('response.statusCode === 200') {
     let i = 0;
     const resultt = JSON.parse(body).results;
+    const actor = '/api/people/18/';
     for (let j = 0; j < resultt.length - 1; j++) {
-      if (resultt[i].characters.find(x => x.includes('https://swapi-api.hbtn.io/api/people/18/'))) {
+      if (resultt[i].characters.find(x => x.includes(actor))) {
         i++;
       }
     }
