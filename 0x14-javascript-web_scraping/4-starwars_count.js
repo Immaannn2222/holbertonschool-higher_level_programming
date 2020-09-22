b0d36d866ea13256c43158ed96fa996a9b8d1c96
Immaannn2222 <imennaayari@gmail.com>
2020-09-22 16:25:06 +0100

    better version

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
-const U_R_L = 'https://swapi-api.hbtn.io/api/films/';
+const U_R_L = 'https://swapi-api.hbtn.io/api/films';
 request(U_R_L, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else if (response.statusCode === 200) {
+  if (!error && response.statusCode === 200) {
     let i = 0;
     const resultt = JSON.parse(body).results;
     for (let j = 0; j < resultt.length; j++) {
-      if (resultt[i].characters.find(el => el.includes('/api/people/18/'))) {
+      if (resultt[i].characters.find(x => x.includes('/api/people/18/'))) {
         i++;
       }
     }
