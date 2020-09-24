1c65e5a6d3632f7e28803ebb2699229390883ec7
Immaannn2222 <imennaayari@gmail.com>
2020-09-24 19:02:12 +0100

    adv late

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,9 +3,9 @@ const request = require('request');
 const address = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request.get(address, function (error, response, body) {
   if (!error && response.statusCode === 200) {
-    const cast = JSON.parse(body.characters);
+    const cast = JSON.parse(body).characters;
     for (let j = 0; j < cast.length; j++) {
-      request.get(JSON.parse(body).characters[j], function (e, r, b) {
+      request.get(cast[j], function (e, r, b) {
         console.log(JSON.parse(b).name);
       });
     }
